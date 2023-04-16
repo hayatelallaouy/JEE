351f36fcaa1a47b770386ddd475ddcdc67c912d6
hayatelallaouy <hayatelallaouy01@gmail.com>
2023-04-16 23:53:34 +0200

    jee

--- patients-mvc/src/main/java/ma/emsi/patientsmvc/PatientsMvcApplication.java
+++ patients-mvc/src/main/java/ma/emsi/patientsmvc/PatientsMvcApplication.java
@@ -6,28 +6,32 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
-
 @SpringBootApplication
 public class PatientsMvcApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PatientsMvcApplication.class, args);
     }
-
-    @Bean
+    //@Bean
     CommandLineRunner commandLineRunner(PatientRepository patientRepository){
         return args -> {
-            patientRepository.save(new Patient(null, "Hiba", new Date(), false, 12));
+            patientRepository.save(new Patient(null, "Hiba", new Date(), false, 112));
             patientRepository.save(new Patient(null, "Salim", new Date(), true, 321));
-            patientRepository.save(new Patient(null, "Aya", new Date(), true, 65));
-            patientRepository.save(new Patient(null, "Farid", new Date(), false, 32));
+            patientRepository.save(new Patient(null, "Aya", new Date(), true, 165));
+            patientRepository.save(new Patient(null, "Farid", new Date(), false, 132));
 
             patientRepository.findAll().forEach(p->{
                 System.out.println(p.getNom());
             });
 
         };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
